f0ea975596ca4533f809258b9ee7d493d7846640
Dor <cohensdor@gmail.com>
2015-10-13 13:45:21 +0300

    13.10 13:44 - add quiz

--- routes/quizz.js
+++ routes/quizz.js
@@ -4,17 +4,27 @@
 var express = require('express');
 var router = express.Router();
 var mySql = require('./../DAL/mySql.js');
+var tmp;
 
 router.post('/', function(req, res)
 {
     quiz_name=req.body.name;
+    tmp=req.body;
     categ=req.body.category;
     mySql.addQuizz("'"+quiz_name.toString()+"',0,8,"+categ.toString());
     mySql.getQuizzId(quiz_name,regQuestions);
 
 });
 function regQuestions(err,data){
-    console.log(data);
+    q_id=data[0].quiz_id;
+    for(var i=0;i<=tmp.qc;i++){
+        text=tmp['q'+i+'text'];
+        corr=tmp['q'+i+'correct'];
+        ansB=tmp['q'+i+'answerB'];
+        ansC=tmp['q'+i+'answerC'];
+        ansD=tmp['q'+i+'answerD'];
+        console.log([text,corr,ansB,ansC,ansD]);
+    }
+
 }
 module.exports = router;
-
